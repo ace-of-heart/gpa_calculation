f7440cc68a74c158b0868accb7cf44db35eebe57
D A R K <42583742+ace-of-heart@users.noreply.github.com>
2024-04-11 15:48:37 +0000

    new pages added and field page edited

--- app.py
+++ app.py
@@ -4,9 +4,29 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def indexPage():
   return render_template('home.html')
 
+@app.route('/home')
+def homePage():
+  return render_template('home.html')
+
+@app.route('/fields')
+def history():
+  return render_template('fields.html')
+
+# @app.route('/translate')
+# def translate():
+#   return render_template('translate.html')
+
+@app.route('/faqs')
+def faqs():
+  return render_template('faqs.html')
+
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug= True)
+  app.run(host='0.0.0.0', debug=True)
 
